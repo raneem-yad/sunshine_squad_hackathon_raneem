5ac6414f2af5df27838b2e601eb89ce2d236e434
felipesandoli <cfelipesandoli@gmail.com>
2024-03-21 22:44:34 +0000

    Set up routing for article details page

--- appmanager/routes.py
+++ appmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, session
 from appmanager import app
 from appmanager.server.articles import get_articles_from_api
 
@@ -8,4 +8,12 @@ from appmanager.server.articles import get_articles_from_api
 def home(page_index=0):
     page = page_index
     data = get_articles_from_api(page)
+    session['articles'] = data
+    print(session)
     return render_template("home.html", result=data, total_pages=10, page=page)
+
+@app.route("/article/<int:article_id>")
+def article_details(article_id):
+    session.get('articles')
+    print(session.get('articles'))
+    return render_template("article-details.html", article_id=article_id)
