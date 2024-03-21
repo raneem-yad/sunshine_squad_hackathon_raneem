c50008fb6ccb1749ef03ba82929342a5b0c0d495
felipesandoli <cfelipesandoli@gmail.com>
2024-03-21 23:13:11 +0000

    Add articles to session and get article clicked on to display it's details

--- appmanager/__init__.py
+++ appmanager/__init__.py
@@ -1,11 +1,16 @@
 import os
 from flask import Flask
+from flask_session import Session
 
 if os.path.exists("env.py"):
     import env  # noqa #no quilty assurance
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+# Use server side session, client side storage has size limitation
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 
 
--- appmanager/routes.py
+++ appmanager/routes.py
@@ -8,12 +8,12 @@ from appmanager.server.articles import get_articles_from_api
 def home(page_index=0):
     page = page_index
     data = get_articles_from_api(page)
+    # stores all articles in session
     session['articles'] = data
-    print(session)
     return render_template("home.html", result=data, total_pages=10, page=page)
 
 @app.route("/article/<int:article_id>")
 def article_details(article_id):
-    session.get('articles')
-    print(session.get('articles'))
-    return render_template("article-details.html", article_id=article_id)
+    # gets especific article from session with index from article clicked
+    article = session.get("articles", {})[article_id]
+    return render_template("article-details.html", article=article)
