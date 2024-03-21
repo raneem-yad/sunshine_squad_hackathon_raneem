ce947e5e3aa882f9956e49860e88751a6e8e0494
raneem-yad <eng.raneem.yad@gmail.com>
2024-03-21 01:44:22 +0100

    get articles from API

---
+++ appmanager/utils/constants.py
@@ -0,0 +1,17 @@
+happy_keywords = [
+    "Happiness",
+    "Joy",
+    "Celebration",
+    "Success",
+    "Inspiration",
+    "Triumph",
+    "Hope",
+    "Kindness",
+    "Love",
+    "Goodwill",
+    "Generosity",
+    "Accomplishment",
+    "Gratitude",
+    "Positivity",
+    "Bright side"
+]
