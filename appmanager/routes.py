f29ab40e573236a86b6f9ee5ca8ea467a1905e7e
raneem-yad <eng.raneem.yad@gmail.com>
2024-03-22 13:00:04 +0100

    fix dependency issue

--- appmanager/routes.py
+++ appmanager/routes.py
@@ -10,17 +10,16 @@ def home(page_index=0):
     data = get_articles_from_api(page)
     # stores all articles in session
     session['articles'] = data
-
     return render_template("home.html", result=data, total_pages=10, page=page)
 
-  
+
 @app.route("/game")
 def game():
     return render_template("game-page.html")
-  
+
+
 @app.route("/article/<int:article_id>")
 def article_details(article_id):
     # gets especific article from session with index from article clicked
     article = session.get("articles", {})[article_id]
     return render_template("article-details.html", article=article)
-
