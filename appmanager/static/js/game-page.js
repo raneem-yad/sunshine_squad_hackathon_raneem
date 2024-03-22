d71328792473da8117f0bc69bb56b5ffa288fc1a
AlexSmall96 <alex.small739@gmail.com>
2024-03-22 19:07:14 +0000

    Finalise game logic

--- appmanager/static/js/game-page.js
+++ appmanager/static/js/game-page.js
@@ -1,9 +1,17 @@
 document.addEventListener("DOMContentLoaded", () =>{
     // Declare const variables
     const gameCells = document.getElementsByClassName("game-cell")
+    const gameTable = document.getElementById('game-table')
     const message = document.getElementById('message')
     const score = document.getElementById('score')
     const newGameBtn = document.getElementById('new-game')
+
+    // Taken from https://alvarotrigo.com/blog/wait-1-second-javascript/
+    function delay(milliseconds){
+        return new Promise(resolve => {
+            setTimeout(resolve, milliseconds);
+        });
+    }
 
     // Function to detect win based on game board and last played position
     const detectWin = (row, col) => {
@@ -62,6 +70,7 @@ document.addEventListener("DOMContentLoaded", () =>{
         [0, 0, 0],
         [0, 0, 0]
     ]
+    let gameRunning = true
 
     // Add event listener to new game button
     newGameBtn.addEventListener("click", () => {
@@ -80,38 +89,71 @@ document.addEventListener("DOMContentLoaded", () =>{
         }
         // Re-disable new game button
         newGameBtn.disabled = true
+        message.innerText = ''
         count = 0
+        gameRunning = true
+        usersTurn = true
+        gameTable.classList.remove('hidden')
     })
 
     // Add event listener to each square
     for (let cell of gameCells){
-        cell.addEventListener("click", () => {
+        cell.addEventListener("click", async () => {
             // Only let user click if cell empty
-            if (!cell.classList.contains('disabled')){
+            if (!cell.classList.contains('disabled') && gameRunning && usersTurn){
                 // Make emoji appear
-                cell.style.color = usersTurn ? ('green'):('blue')
+                cell.style.color = 'green'
                 cell.classList.add('disabled')
-                cell.childNodes[0].classList.add(
-                    `fa-face-${usersTurn ? ('smile'):('frown-open')}`
-                )
+                cell.childNodes[0].classList.add('fa-face-smile')
                 // Get last played cell and check for win
                 let row = parseInt(cell.id.substring(0, 1))
                 let col = parseInt(cell.id.substring(2, 3))
-                gameBoard[row][col] = usersTurn ? (1):(-1)
+                gameBoard[row][col] = 1
                 count++
                 let feedback = detectWin(row, col)
-                if (feedback === 'comp-win' || feedback === 'user-win'){
-                    message.innerText = feedback
+                usersTurn=false
+                // Check is user has won
+                if (feedback === 'user-win'){
+                    message.innerText = ('Well Done! You won')
                     // Increment score if user has won
-                    if (feedback === 'user-win'){
-                        score.innerText = parseInt(score.innerText) + 1
-                    }
+                    score.innerText = parseInt(score.innerText) + 1
                     newGameBtn.disabled = false
+                    gameRunning = false
+                // Game is a draw
                 } else if (count === 9){
                     message.innerText = 'Draw'
                     newGameBtn.disabled = false
+                    gameRunning = false
+                // Game still in progress
+                } else {
+                    message.innerText = 'Computer is thinking...'
+                    await(delay(1000))
+                    // It is now computers turn
+                    let compRow = Math.floor(Math.random() * 3);
+                    let compCol = Math.floor(Math.random() * 3);
+                    // If square already taken find another
+                    while (gameBoard[compRow][compCol] != 0){
+                        compRow = Math.floor(Math.random() * 3);
+                        compCol = Math.floor(Math.random() * 3);
+                    }
+                    gameBoard[compRow][compCol] = -1
+                    count++
+                    let chosenCell = document.getElementById(`${compRow}-${compCol}`)
+                    chosenCell.style.color = 'blue'
+                    chosenCell.classList.add('disabled')
+                    chosenCell.childNodes[0].classList.add('fa-face-frown-open')
+                    // Check is computer has won
+                    if (detectWin(row, col) === 'comp-win'){
+                        message.innerText = ('Sorry, computer won. Press new game button to try again.')
+                    } else if (count === 9){
+                        message.innerText = 'Draw'
+                        newGameBtn.disabled = false
+                        gameRunning = false
+                    } else {
+                        usersTurn = true
+                        message.innerText = ''
+                    }
                 }
-                usersTurn = !usersTurn
             }
         })
     }
